d81415080d1a3706209796a17e5a1777b58aa343
Andika Alifian <andfian05@gmail.com>
2023-09-01 11:10:37 +0700

    ResikRek
    Application Mobile + Laravel

---
+++ resources/views/admin/data/datatables.blade.php
@@ -0,0 +1,157 @@
+<!doctype html>
+<html lang="en">
+
+{{-- Header.blade.php --}}
+@include('admin.layout.header')
+
+<body>
+  <!--  Body Wrapper -->
+  <div class="page-wrapper" id="main-wrapper" data-layout="vertical" data-navbarbg="skin6" data-sidebartype="full"
+    data-sidebar-position="fixed" data-header-position="fixed">
+    
+
+    {{-- Sidebar.blade.php --}}
+    @include('admin.layout.sidebar')
+
+
+
+    <!--  Main wrapper -->
+    <div class="body-wrapper">
+      <!--  Header Start -->
+      <header class="app-header">
+         {{-- Navbar.blade.php --}}
+         @include('admin.layout.navbar')
+      </header>
+      
+
+      <div class="container-fluid">
+        <div class="container-fluid">
+          <div class="card">
+            <div class="card-body">
+            
+              <h5 class="card-title fw-semibold mb-4 text-center">Data Lingkungan</h5>
+              <button type="button" class="btn btn-secondary m-1 mb-3"><i class="fa-solid fa-plus"></i>&nbsp; Tambah Data</button>
+              <button type="button" class="btn btn-info m-1 mb-3"><i class="fa-solid fa-file-pdf"></i>&nbsp; Export .PDF</button>
+              <div class="card">
+                <div class="card-body">
+                  <form>
+                    
+                    <div class="table-responsive " id="no-more-tables">
+                    <table class="table">
+                        <thead>
+                          <tr>
+                            <th scope="col" class="text-center">No</th>
+                            <th scope="col">Title</th>
+                            <th scope="col">Author</th>
+                            <th scope="col">Description</th>
+                           
+                            <th scope="col" class="text-center">Action</th> 
+                          </tr>
+                        </thead>
+                        <tbody class="table-group-divider">
+                          
+                            <tr>
+                              <td scope="row" data-title="No" class="text-center">1</td>
+                              <td data-title="Title">Lapangan</td>
+                              <td data-title="Author">Firman</td>
+                              <td data-title="Description">Membersihkan Lapangan</td>
+                              <th class="d-flex justify-content-center">
+                                <a class="btn btn-primary btn-sm me-2"
+                                    href=""><i class="fa-sharp fa-solid fa-magnifying-glass"></i>  Detail</a>
+                                <a class="btn btn-success btn-sm me-2"
+                                    href=""><i class="fa-solid fa-pencil"></i> Update</a>
+                                <form method="POST" action=""
+                                    style="display: inline-block;">
+                                  
+                                    <button class="btn btn-danger btn-sm"
+                                        onclick="return confirm('Delete?')"><i class="fa-solid fa-trash"></i>  Delete</button>
+                                </form>
+                              </th>
+                            </tr>
+                            
+                        </tbody>
+                        
+                      </table>
+                    </div>
+    
+                  </form>
+                </div>
+              </div>
+    
+              <br><br>
+              <hr>
+    
+              <h5 class="card-title fw-semibold mb-4 text-center">Data Pengguna</h5>
+              <button type="button" class="btn btn-secondary m-1 mb-3"><i class="fa-solid fa-plus"></i>&nbsp; Tambah Data</button>
+              <button type="button" class="btn btn-info m-1 mb-3"><i class="fa-solid fa-file-pdf"></i>&nbsp; Export .PDF</button>
+              <div class="card">
+                <div class="card-body">
+                  <form>
+                    
+                    <div class="table-responsive " id="no-more-tables">
+                    <table class="table">
+                        <thead>
+                          <tr>
+                            <th scope="col" class="text-center">No</th>
+                            <th scope="col">Full Name</th>
+                            <th scope="col">Email</th>
+                            <th scope="col">Status</th>
+                           
+                            <th scope="col" class="text-center">Action</th> 
+                          </tr>
+                        </thead>
+                        <tbody class="table-group-divider">
+                         
+                            
+                            <tr>
+                              <td scope="row" data-title="No" class="text-center">1</td>
+                              <td data-title="Full Name">Andika</td>
+                              <td data-title="Email">dummy@gmail.com</td>
+                              <td data-title="Status">Selesai</td>
+                              <th class="d-flex justify-content-center">
+                                <a class="btn btn-primary btn-sm me-2"
+                                    href=""><i class="fa-sharp fa-solid fa-magnifying-glass"></i>  Detail</a>
+                                <a class="btn btn-success btn-sm me-2"
+                                    href=""><i class="fa-solid fa-pencil"></i> Update</a>
+                                <form method="POST" action=""
+                                    style="display: inline-block;">
+                                  
+                                    <button class="btn btn-danger btn-sm"
+                                        onclick="return confirm('Delete?')"><i class="fa-solid fa-trash"></i>  Delete</button>
+                                </form>
+                              </th>
+                            </tr>
+                            
+                        </tbody>
+                        
+                      </table>
+                    </div>
+    
+                  </form>
+                </div>
+              </div>
+    
+    
+             
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+
+
+
+
+
+    </div>
+  </div>
+  
+
+  {{-- Footer.blade.php --}}
+  @include('admin.layout.footer')
+
+
+</body>
+
+</html>
---
+++ resources/views/admin/layout/sidebar.blade.php
@@ -0,0 +1,75 @@
+<!-- Sidebar Start -->
+<aside class="left-sidebar">
+    <!-- Sidebar scroll-->
+    <div>
+      <div class="brand-logo d-flex align-items-center justify-content-between">
+        <a href="./index.html" class="text-nowrap logo-img">
+          <img src="{{asset('icon/resik-logo.png')}}" class="rounded mx-auto d-block" width="180" alt="">
+        </a>
+        <div class="close-btn d-xl-none d-block sidebartoggler cursor-pointer" id="sidebarCollapse">
+          <i class="ti ti-x fs-8"></i>
+        </div>
+      </div>
+      <!-- Sidebar navigation-->
+      <nav class="sidebar-nav scroll-sidebar" data-simplebar="">
+        <ul id="sidebarnav">
+          <li class="nav-small-cap">
+            <i class="ti ti-dots nav-small-cap-icon fs-4"></i>
+            <span class="hide-menu">Home</span>
+          </li>
+
+          <li class="sidebar-item">
+            <a class="sidebar-link" href="/dashboard" aria-expanded="false">
+              <span>
+                <i class="ti ti-layout-dashboard"></i>
+              </span>
+              <span class="hide-menu">Dashboard</span>
+            </a>
+          </li>
+
+          <li class="nav-small-cap">
+            <i class="ti ti-dots nav-small-cap-icon fs-4"></i>
+            <span class="hide-menu">Data Global</span>
+          </li>
+          <li class="sidebar-item">
+            <a class="sidebar-link" href="/datatables" aria-expanded="false">
+              <span>
+                <i class="ti ti-article"></i>
+              </span>
+              <span class="hide-menu">Datatables</span>
+            </a>
+          </li>
+          <li class="sidebar-item">
+            <a class="sidebar-link" href="./ui-alerts.html" aria-expanded="false">
+              <span>
+                <i class="ti ti-alert-circle"></i>
+              </span>
+              <span class="hide-menu">Alerts</span>
+            </a>
+          </li>
+          <li class="sidebar-item">
+            <a class="sidebar-link" href="./ui-card.html" aria-expanded="false">
+              <span>
+                <i class="ti ti-cards"></i>
+              </span>
+              <span class="hide-menu">Card</span>
+            </a>
+          </li>
+          
+        <div class="unlimited-access hide-menu bg-light-primary position-relative mb-7 mt-5 rounded">
+          <div class="d-flex">
+            <div class="unlimited-access-title me-3">
+              <h6 class="fw-semibold fs-4 mb-6 text-dark w-85 mt-3">ResikRek Application</h6>
+              
+            </div>
+            <div class="unlimited-access-img">
+              <img src="{{asset('Templeate/src/assets/images/backgrounds/rocket.png')}}" alt="" class="img-fluid">
+            </div>
+          </div>
+        </div>
+      </nav>
+      <!-- End Sidebar navigation -->
+    </div>
+    <!-- End Sidebar scroll-->
+  </aside>
+  <!--  Sidebar End -->
